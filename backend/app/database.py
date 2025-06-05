1903f4dcbeb09eae9d1eaf01edec4b66bb8a50d0
Dmitry Drozdov <ds.drozdov@gmail.com>
2025-06-05 18:02:39 +0400

    ref

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,6 @@
 from sqlmodel import create_engine, Session, SQLModel
 
-DATABASE_URL = "sqlite:///.tasks.db"
+DATABASE_URL = "sqlite:///../data/tasks.db"
 engine = create_engine(DATABASE_URL, echo=True)
 
 
